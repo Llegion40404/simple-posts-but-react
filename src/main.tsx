8ca1438364f9b551de177b3e87ef8e7206b68844
Alex Hmelevcev <hmelevcev.aleksandr@sarkor.uz>
2024-06-26 15:23:27 +0500

    project-setup completed

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './pages/error-page';
+import CreatePage from './pages/create';
+import PostList from './pages/postslist';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		Component: App,
+		errorElement: <ErrorPage />,
+		children: [
+			{ path: '/create/:postId?', Component: () => <CreatePage /> },
+			{ path: '/', Component: () => <PostList /> },
+		],
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>,
+);
